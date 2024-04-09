ae3627a36659c9b8a9a1b751984ec1051906f93e
Astrid Geels <moerastrid@gmail.com>
2024-04-09 14:28:09 +0200

    setup some basic components

--- frontend/src/app/services/test.service.ts
+++ frontend/src/app/services/test.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 
 export interface Test {
   id: number,
@@ -12,9 +12,6 @@ export interface Test {
   providedIn: 'root',
 })
 export class TestService {
-
-
-
   constructor(private http: HttpClient) { };
 
   getTest() {
