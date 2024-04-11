d175fc9f08fe2dda403c8a11004dd446dc8a29ff
Astrid Geels <moerastrid@gmail.com>
2024-04-11 15:14:09 +0200

    scss file structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { SettingsComponent } from './components/settings/settings.component';
 import { WelcomeComponent } from './components/welcome/welcome.component';
-import { FriendsComponent } from './friends/friends.component';
+import { FriendsComponent } from './components/friends/friends.component';
 // import { WelcomeComponent } from '/src/app/components/welcome/welcome.component'
 
 export const routes: Routes = [
