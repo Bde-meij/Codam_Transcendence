e42fb9616961120418b87d6845d6f81c5cf0318d
Astrid Geels <moerastrid@gmail.com>
2024-04-23 15:08:42 +0200

    made authservice a standalone service and implement it in applogin

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,36 +1,28 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Injectable } from '@angular/core';
-import { Auth0Client, createAuth0Client } from '@auth0/auth0-spa-js';
-import { User } from '../../models/user.class';
+import { WebAuth } from 'auth0-js';
+
 // import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-	// private authClient: Auth0Client | undefined;
+	private auth: WebAuth;
 
-	// constructor() {
-	// 	this.initializeAuth0();
-	// };
+	constructor() {
+		this.auth = new WebAuth({
+			domain: 'http://api.intra.42.fr/v2/oauth',
+			clientID: 'u-s4t2ud-7ae181090888396e717cc9cdec0e0ff9a312c655e22ea37b1cc2e426536847cb',
+			redirectUri: window.location.origin,
+			responseType: 'code',
+			scope: 'public',
+		})
+	};
 
-	// async initializeAuth0() {
-	// 	this.authClient = await createAuth0Client({
-	// 		domain: 'http://api.intra.42.fr/v2/oauth',
-	// 		clientId: 'u-s4t2ud-7ae181090888396e717cc9cdec0e0ff9a312c655e22ea37b1cc2e426536847cb',
-	// 		authorizationParams: {
-	// 			redirect_uri: window.location.origin,
-	// 			scope: 'public'
-	// 		}
-	// 	});
-	// }
+	login() : void {
+		this.auth.authorize();
+	}
 
-	// public async login() {
-	// 	if (this.authClient) {
-	// 		await this.authClient.loginWithRedirect();
-	// 	} else {
-	// 		this.initializeAuth0();
-	// 	}
-	// }
-
+	
 }
