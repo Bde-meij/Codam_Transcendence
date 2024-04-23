c7304fe8acc8abe54f875afd1ca6d8fbf2b988df
Astrid Geels <moerastrid@gmail.com>
2024-04-23 15:46:16 +0200

    get code from auth0 api and send it to backend

--- frontend/src/app/services/auth/auth.service.ts
+++ frontend/src/app/services/auth/auth.service.ts
@@ -1,4 +1,5 @@
 
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { WebAuth } from 'auth0-js';
 
@@ -9,12 +10,13 @@ import { WebAuth } from 'auth0-js';
 })
 export class AuthService {
 	private auth: WebAuth;
+	private authApi = "/api/auth";
 
-	constructor() {
+	constructor(private http: HttpClient) {
 		this.auth = new WebAuth({
 			domain: 'http://api.intra.42.fr/v2/oauth',
 			clientID: 'u-s4t2ud-7ae181090888396e717cc9cdec0e0ff9a312c655e22ea37b1cc2e426536847cb',
-			redirectUri: window.location.origin,
+			redirectUri: "http://localhost:4200/callback",
 			responseType: 'code',
 			scope: 'public',
 		})
@@ -22,7 +24,9 @@ export class AuthService {
 
 	login() : void {
 		this.auth.authorize();
-	}
-
+	};
 	
+	sendCode(code: string) : void {
+		this.http.post(this.authApi + '/code', code);
+	}
 }
