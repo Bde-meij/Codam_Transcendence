2b724097c93f27b2524c0bbbeb4723afb3a9029a
Emily Winnink <emily.winnink@gmail.com>
2024-04-29 18:40:57 +0200

    Added server logging of connections

--- api/src/chat/chat.gateway.ts
+++ api/src/chat/chat.gateway.ts
@@ -1,24 +1,45 @@
 import {
-  ConnectedSocket,
-  MessageBody,
-  SubscribeMessage,
-  WebSocketGateway,
+	ConnectedSocket,
+	MessageBody,
+	OnGatewayConnection,
+	OnGatewayDisconnect,
+	OnGatewayInit,
+	SubscribeMessage,
+	WebSocketGateway,
+	WebSocketServer,
 } from '@nestjs/websockets';
-import { Socket } from 'socket.io';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway({
-  cors: { origin: 'http://localhost:4200' },
-  namespace: '/api/chat-socket',
-  path: '/api/chat-socket/socket.io',
+	cors: { origin: 'http://localhost:4200' },
+	namespace: '/api/chat-socket',
+	path: '/api/chat-socket/socket.io',
 })
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(
-    @MessageBody() data: string,
-    @ConnectedSocket() client: Socket,
-  ): string {
-    console.log('message recieved: ' + data);
-    client.broadcast.emit('message', data);
-    return 'Hello world!';
-  }
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+
+	@WebSocketServer() io: Server;
+
+	afterInit() {
+		console.log("server initialized");
+	}
+
+	handleConnection(client: any, ...args: any[]) {
+		const { sockets } = this.io.sockets;
+
+		console.log("user " + client.id + " connected");
+	}
+	
+	handleDisconnect(client: any) {
+		console.log("user " + client.id + " disconnected");
+	}
+
+	@SubscribeMessage('message')
+	handleMessage(
+		@MessageBody() data: string,
+		@ConnectedSocket() client: Socket,
+	): string {
+		console.log('message recieved from ' + client.id + ': ' + data);
+		client.broadcast.emit('message', data);
+		return 'Hello world!';
+	}
 }
