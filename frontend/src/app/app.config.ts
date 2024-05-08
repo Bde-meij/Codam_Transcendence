97327256c1c70ed767668cfc7e6edd92bca2280e
Astrid Geels <moerastrid@gmail.com>
2024-05-08 16:58:22 +0200

    temp cookie in dash

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { AuthService } from './services/auth/auth.service';
 import { provideHttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(routes),
 		provideHttpClient(),
 		{ provide: AuthService },
+		{ provide: CookieService }
 	]
 };
--- frontend/src/app/components/dashboard/dashboard.component.ts
+++ frontend/src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,7 @@ import { UserInterface } from '../../models/user.class';
 import { UserService } from '../../services/user/user.service';
 import { AsyncPipe, JsonPipe } from '@angular/common';
 import { SockService } from '../../services/sock/sock.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-dashboard',
@@ -21,13 +22,20 @@ export class DashboardComponent implements OnInit {
 
 	user$ : Observable<UserInterface> | undefined;
 
-	unread = false;
+	unread = this.chatService.isUnread();
 
-	constructor(private userService: UserService, private chatService: ChatService) {
-		this.unread = chatService.isUnread();
+	constructor(private userService: UserService, private chatService: ChatService, private cookieService: CookieService) {
+		// Set cookie
+		this.cookieService.set('cookieName', 'koekje');
+
+		// Get cookie
+		const cookieValue = this.cookieService.get('connect.sid');
+		console.log('Cookie value:', cookieValue);
 	};
 
 	ngOnInit(): void {
 		this.user$ = this.userService.getUser();
 	}
+
+	// testvalue = window.Cookies
 }
