7dc5fce8652673453dc9d726cbaec235609faa34
Astrid Geels <moerastrid@gmail.com>
2024-05-21 16:10:35 +0200

    removed unnessecary functions from game.service.ts

--- frontend/src/app/services/sock/game/game.service.ts
+++ frontend/src/app/services/sock/game/game.service.ts
@@ -9,34 +9,14 @@ import { SockService } from '../sock.service';
 export class GameService{
 	private gameSocket = io("/game");
 
-	private unread = false;
-
 	constructor(sockService: SockService) {
+		// for debugging:
 		this.gameSocket.onAny((event, ...args) => {
-			console.log("game-SOCK EVENT: ");
+			console.log("GAME-SOCK EVENT: ");
 			console.log(event, args);
 		});
 		sockService.newSocketRegister("gameSocket");
 	}
 
-	sendMessage(message: string): void {
-		this.gameSocket.emit('message', message, (err: any) => {
-			if (err) {
-				console.log("game-sock error: ");
-				console.log(err);
-			}
-		});
-	}
-
-	getMessages(): Observable<string> {
-		return new Observable((observer) => {
-			this.gameSocket.on('message', (message) => {
-				observer.next(message);
-			});
-		});
-	}
-	 
-	isUnread() {
-		return this.unread;
-	}
+	// other functionality
 }
