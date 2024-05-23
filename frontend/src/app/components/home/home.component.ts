f0236edc303665b3afee7c92430c326ce7c276bf
moerastrid@gmail.com <moerastrid@gmail.com>
2024-05-23 14:18:48 +0200

    restyling the home page, made buttons that link to game and settings so far

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,14 +1,15 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-	height = 600;
+	height = window.innerHeight - 10;
 	width = this.height * 1;
 }
