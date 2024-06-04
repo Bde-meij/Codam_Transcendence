28c3f3d6ab81f366ceb05ebff530beb8b2cf3256
Francisco Purdom <fddp2008@hotmail.com>
2024-06-04 17:51:49 +0200

    made 2fa settings pretty

--- frontend/src/app/components/settings/settings.component.ts
+++ frontend/src/app/components/settings/settings.component.ts
@@ -14,21 +14,35 @@ export class SettingsComponent {
   constructor(private http: HttpClient) {};
 
   isChecked: boolean = false;
+  is2faEnabled: boolean = false;
   qrCode: string = "";
   secret: string = "";
   userInput: string = "";
+  verificationRes: string = "";
+
+  ngOnInit(): void {
+    this.http.get<any>("/api/auth/is2faenabled").subscribe(data => {
+      this.is2faEnabled = data.isTwoFAEnabled;
+    })
+  }
 
   onChange() {
-    if (this.isChecked) {
+    if (this.is2faEnabled) {
+      this.isChecked = true;
       this.http.get<any>("/api/auth/2fasetup").subscribe( data => {
         this.qrCode = data.qrCode;
         this.secret = data.secret;
     });
     }
+    else {
+      this.isChecked = false;
+      this.http.post<any>('/api/auth/2fadisable', {}).subscribe();
+    }
   }
 
   verifyUserInput() {
-    this.http.post('/api/auth/2faverify', { userInput: this.userInput }).subscribe(response => {
+    this.http.post<any>('/api/auth/2faverify', { userInput: this.userInput }).subscribe(response => {
+      this.verificationRes = response.message;
       console.log('Verification response:', response);
     }, error => {
       console.error('Error verifying user input:', error);
