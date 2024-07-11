f81e5ac6053670e6d18253d153aca056f7bb9a4a
moerastrid@gmail.com <moerastrid@gmail.com>
2024-07-11 17:07:00 +0200

    temp

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -61,7 +61,7 @@ export class AuthController {
 		}
 		else {
 			this.userService.updateStatus(user.id, 'online');
-			res.status(HttpStatus.FOUND).redirect(`http://${req.hostname}:4200/dashboard`);
+			res.status(HttpStatus.FOUND).redirect(`http://${req.hostname}:4200/dashboard/home`);
 		}
 	}
 
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -9,7 +9,31 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-	height = window.innerHeight - 10;
-	width = this.height * 1;
+export class HomeComponent implements OnInit{
+	height : number = 400;
+	width : number = 400;
+
+	ngOnInit(): void {
+		this.screenResize();
+	}
+
+	onResize(event: any) {
+		console.log("event: ", event);
+		// window.innerWidth = event.target.innerWidth;
+		// window.innerHeight = event.target.innerHeight;
+		this.screenResize();
+	}
+
+	screenResize()
+	{
+		if (window.innerWidth < window.innerHeight) {
+			console.log("innerWidth smallest");
+			this.width = window.innerWidth - 10, 
+			this.height = window.innerWidth - 10;
+		} else {
+			console.log("innerHeight smallest");
+			this.width = window.innerHeight - 10;
+			this.height = window.innerHeight - 10;
+		}
+	}
 }
