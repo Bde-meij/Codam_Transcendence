d14aacff5ca4ea7e88d992ec673a7c89f3425c97
Bastian de Meijer <bde-meij@student.codam.nl>
2024-08-29 16:25:24 +0200

    added protection

--- frontend/src/app/components/game-menu/game-menu.component.ts
+++ frontend/src/app/components/game-menu/game-menu.component.ts
@@ -14,23 +14,63 @@ export class GameMenuComponent {
     constructor(private router: Router, private userService: UserService) {};
 
     pongGame() {
-      this.userService.updateRoomKey(0).subscribe();
-      this.router.navigate(['/dashboard/game'])
+      this.userService.updateRoomKey(0).subscribe(
+      {
+        next: () =>
+        {
+          this.router.navigate(['/dashboard/game']);
+        },
+        error: () =>
+        {
+          console.log("error: failed to update 'roomkey'")
+        }
+      }
+    );
     }
     
     flappyGame() {
-      this.userService.updateRoomKey(-1).subscribe();
-      this.router.navigate(['/dashboard/game'])
+      this.userService.updateRoomKey(-1).subscribe(
+        {
+          next: () =>
+          {
+            this.router.navigate(['/dashboard/game']);
+          },
+          error: () =>
+          {
+            console.log("error: failed to update 'roomkey'")
+          }
+        }
+      );
     }
 
     bossPongSingle() {
-      this.userService.updateRoomKey(-1).subscribe();
-      this.router.navigate(['/dashboard/bossPong'])
+      this.userService.updateRoomKey(-1).subscribe(
+        {
+          next: () =>
+          {
+            this.router.navigate(['/dashboard/bossPong']);
+          },
+          error: () =>
+          {
+            console.log("error: failed to update 'roomkey'")
+          }
+        }
+      );
     }
 
     bossPongMulti() {
-      this.userService.updateRoomKey(1).subscribe();
-      this.router.navigate(['/dashboard/bossPong'])
+      this.userService.updateRoomKey(1).subscribe(
+        {
+          next: () =>
+          {
+            this.router.navigate(['/dashboard/bossPong']);
+          },
+          error: () =>
+          {
+            console.log("error: failed to update 'roomkey'")
+          }
+        }
+      );
     }
 }
 
